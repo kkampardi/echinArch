46fdb8ad68922cc0fd0c2c50ce2ee45a2146cab2
Katerina Kampardi <kkampardi@gmail.com>
2016-02-10 14:52:12 +0200

    #Remove blog app

--- portfolio/models.py
+++ portfolio/models.py
@@ -33,7 +33,6 @@ class Project(models.Model):
     def __str__(self):
         return self.name
 
-
 class Skill(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio',
-    'blog',
+    #'blog',
 ]
 
 MIDDLEWARE_CLASSES = [
