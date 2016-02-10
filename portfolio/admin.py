efa3e13c458c25dff1e6ba2eb8499aa7626035dd
Katerina Kampardi <kkampardi@gmail.com>
2016-02-10 11:40:00 +0200

    #Add Blog app and Portfolio App

---
+++ portfolio/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from models import Project
+from models import Category
+from models import Skill
+# Register your models here.
+admin.site.register(Project)
+admin.site.register(Category)
+admin.site.register(Skill)
