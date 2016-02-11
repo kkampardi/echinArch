93d24719141b4c72ceec94f32b787f4cb471b4ed
Katerina Kampardi <kkampardi@gmail.com>
2016-02-11 15:08:41 +0200

    #Added view for portfolio project details

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 if settings.DEBUG:
     urlpatterns = [
         url(r'^$', views.portfolio_list, name='portfolio_list'),
+        url(r'^project/(?P<pk>[0-9]+)/$', views.portfolio_detail, name='portfolio_detail'),
+        url(r'^about$', views.about, name='about'),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, render_to_response
 from django.utils import timezone
 from .models import Project
 
+
+def about(request):
+    return render_to_response('about.html', {})
+
 def portfolio_list(request):
     projects = Project.objects.filter(start_date__lte=timezone.now()).order_by('start_date')
-    return render(request, 'portfolio/portfilio_list.html', {'projects': projects})
+    return render(request, 'portfolio/portfolio_list.html', {'projects': projects})
+
+def portfolio_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, 'portfolio/portfolio_detail.html', {'project': project})
