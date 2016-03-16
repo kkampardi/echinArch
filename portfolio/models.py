bbe5a1fd126c09a0568367038e57af4121492eaf
Katerina Kampardi <kkampardi@gmail.com>
2016-03-16 12:13:50 +0200

    #add django admin WYSIWYG

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,21 +1,19 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
-
+from ckeditor.fields import RichTextField
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=50, unique=True)
-    #url = models.URLField(blank=True, null=True)
-    #pull_quote = models.TextField(blank=True, null=True)
-    short_description = models.TextField()
-    description = models.TextField()
+    short_description = models.CharField(max_length=400)
+    description = RichTextField()
+    clients = models.CharField(max_length=400)
     start_date = models.DateField(blank=True, null=True)
-    end_date = models.DateField(blank=True, null=True)
     image = models.ImageField(upload_to='portfolio')
 
     class Meta:
-        ordering = ['-start_date', '-end_date', ]
+        ordering = ['-start_date', ]
 
     def __unicode__(self):
         return self.name
